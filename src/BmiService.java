b2c581b36fc6868700d2059ef6386fb47f45b830
Tinita <tino4ka.87@mail.ru>
2023-10-11 13:14:14 +0300

    Initial commit

---
+++ src/BmiService.java
@@ -0,0 +1,9 @@
+public class BmiService {
+
+    public int calculate (int kilogram, double meter){
+        double index = kilogram / (meter * meter);
+        {
+        }
+        return (int) index;
+    }
+}
